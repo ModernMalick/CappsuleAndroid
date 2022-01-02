e505e593d49e7e0178ac52b378ff978195eab654
Malick10 <m.ndiaye@intech-sud.fr>
2022-01-02 08:13:47 +0100

    Remove notification, refactor utils

--- app/src/main/java/com/example/cappsule/MainActivity.kt
+++ app/src/main/java/com/example/cappsule/MainActivity.kt
@@ -44,23 +44,6 @@ class MainActivity : AppCompatActivity() {
             val moreDialog = MoreDialog()
             moreDialog.show(supportFragmentManager, "MOREDIALOG")
         }
-
-        createNotificationChannel()
-        val intent = Intent(this, NotificationBroadcast::class.java)
-        val pendingIntent = PendingIntent.getBroadcast(this, 0, intent, 0)
-        val alarmManager = getSystemService(ALARM_SERVICE) as AlarmManager
-        val calendar: Calendar = Calendar.getInstance()
-        calendar.set(Calendar.HOUR_OF_DAY, 7)
-        calendar.set(Calendar.MINUTE, 0)
-        calendar.set(Calendar.SECOND, 0)
-
-        val triggerTime: Long = if (System.currentTimeMillis() > calendar.timeInMillis) {
-            calendar.timeInMillis + AlarmManager.INTERVAL_DAY
-        } else {
-            calendar.timeInMillis
-        }
-
-        alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP, triggerTime, 0, pendingIntent)
     }
 
     private fun onNavigationItemSelected(item: MenuItem): Boolean {
@@ -100,17 +83,6 @@ class MainActivity : AppCompatActivity() {
         }
         supportFragmentManager.beginTransaction().replace(R.id.fragment_container_view, selectedFragment).commit()
         return true
-    }
-
-    private fun createNotificationChannel() {
-            val name = "NOTIFCHANNEL"
-            val importance = NotificationManager.IMPORTANCE_DEFAULT
-            val channel = NotificationChannel("dailyNotifier", name, importance).apply {
-                description = name
-            }
-            val notificationManager: NotificationManager =
-                getSystemService(NOTIFICATION_SERVICE) as NotificationManager
-            notificationManager.createNotificationChannel(channel)
     }
 
     private fun onBoardingFinished(): Boolean{
--- app/src/main/java/com/example/cappsule/Utils.kt
+++ app/src/main/java/com/example/cappsule/Utils.kt
@@ -5,6 +5,7 @@ import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.widget.Toast
 import java.io.ByteArrayOutputStream
+import java.net.URL
 
 fun getBytes(bitmap: Bitmap?): ByteArray {
     val stream = ByteArrayOutputStream()
@@ -20,3 +21,9 @@ fun toaster(context: Context?, string: String?) {
     val toast = Toast.makeText(context, string, Toast.LENGTH_SHORT)
     toast.show()
 }
+
+fun getData(city: String, unit: String): String {
+    return URL("https://api.openweathermap.org/data/2.5/weather?q=$city&units=$unit&appid=$apiKey").readText(Charsets.UTF_8)
+}
+
+const val apiKey = "d6ec42cf87a93d3d280eb8b3e98d8436"
